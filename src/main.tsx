8fcc425513f9f1a56c103bc4f18f660f1341c91f
Islam Yamor <islamyamorz@gmail.com>
2025-11-11 16:46:49 +0300

    added import @/ alias

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import App from './App.tsx'
+import App from '@/App.tsx'
 import { StrictMode } from 'react'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
