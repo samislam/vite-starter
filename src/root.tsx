61f6723f7d5544095e829407a9397285f112d154
Islam Yamor <islamyamorz@gmail.com>
2025-11-12 18:24:52 +0300

    added meta title

--- src/root.tsx
+++ src/root.tsx
@@ -9,6 +9,7 @@ export const Layout = (props: PropsWithChildren) => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>React starter</title>
         <Meta />
         <Links />
       </head>
