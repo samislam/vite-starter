300fe2b656fecbcb6a7262f54cc6c3fae52efb76
Islam Yamor <islamyamorz@gmail.com>
2025-11-12 15:23:20 +0300

    added helper comment

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,6 @@
 import { type RouteConfig, index } from '@react-router/dev/routes'
 
-export default [index('routes/home.page.tsx')] satisfies RouteConfig
+export default [
+  // Define your routes and layouts here...
+  index('routes/home.page.tsx'),
+] satisfies RouteConfig
