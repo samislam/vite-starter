846e593213a3316f09f84826b8b24442792615bd
Islam Yamor <islamyamorz@gmail.com>
2025-11-13 09:31:11 +0300

    removed vite react plugin as reactRouter plugin already includes whats necesary to run react applicaitons

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { reactRouter } from '@react-router/dev/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()],
 })
